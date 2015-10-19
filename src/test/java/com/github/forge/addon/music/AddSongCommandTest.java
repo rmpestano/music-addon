45d7a34c20ae2686e9d9cfdadb7cdd484040fda0
George Gastaldi <gegastaldi@gmail.com>
2015-10-19 02:38:45 -0200

    Fixed failing test

--- src/test/java/com/github/forge/addon/music/AddSongCommandTest.java
+++ src/test/java/com/github/forge/addon/music/AddSongCommandTest.java
@@ -7,65 +7,66 @@
 
 package com.github.forge.addon.music;
 
-import com.github.forge.addon.music.model.Playlist;
-import com.github.forge.addon.music.model.Song;
-import com.github.forge.addon.music.playlist.PlaylistManager;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Inject;
+
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.forge.addon.resource.DirectoryResource;
 import org.jboss.forge.addon.shell.test.ShellTest;
 import org.jboss.forge.addon.ui.result.Failed;
 import org.jboss.forge.addon.ui.result.Result;
-import org.jboss.forge.addon.ui.test.UITestHarness;
 import org.jboss.forge.arquillian.AddonDependencies;
 import org.jboss.forge.arquillian.archive.AddonArchive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import javax.inject.Inject;
-import java.nio.file.Paths;
-import java.util.concurrent.TimeUnit;
-
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
+import com.github.forge.addon.music.model.Playlist;
+import com.github.forge.addon.music.model.Song;
+import com.github.forge.addon.music.playlist.PlaylistManager;
 
 /**
  * @author <a href="antonio.goncalves@gmail.com">Antonio Goncalves</a>
  */
 @RunWith(Arquillian.class)
-public class AddSongCommandTest extends BaseTest{
+public class AddSongCommandTest extends BaseTest {
 
+	@Inject
+	private PlaylistManager manager;
 
-    @Inject
-    private UITestHarness uiTestHarness;
+	@Inject
+	private ShellTest shellTest;
 
-    @Inject
-    private ShellTest shellTest;
+	@Deployment
+	@AddonDependencies
+	public static AddonArchive getDeployment() {
+		return ShrinkWrap.create(AddonArchive.class).addClass(BaseTest.class).addBeansXML();
+	}
 
+	@Test
+	public void shouldAddNewPlaylist() throws Exception {
+		manager.createPlaylist(PlaylistManager.DEFAULT_PLAYLIST);
+		Result result = shellTest.execute("music-add-songs --targetPlaylist " + PlaylistManager.DEFAULT_PLAYLIST
+				+ " --dir " + Paths.get("target/test-classes").toAbsolutePath(), 5, TimeUnit.SECONDS);
+		assertThat(result, not(instanceOf(Failed.class)));
+		assertThat(result.getMessage(),
+				is(equalTo("1 song(s) added to playlist: " + PlaylistManager.DEFAULT_PLAYLIST)));
+		assertThat(playlistManager.hasPlaylist(PlaylistManager.DEFAULT_PLAYLIST), is(true));
+		Playlist playlist = playlistManager.getPlaylist(PlaylistManager.DEFAULT_PLAYLIST);
+		assertNotNull(playlist);
+		Song song = new Song(Paths.get("target/test-classes").toAbsolutePath() + "/axe.mp3");
+		assertThat(playlist.getSongs(), hasItem(song));
 
-
-
-    @Deployment
-    @AddonDependencies
-    public static AddonArchive getDeployment() {
-        return ShrinkWrap.create(AddonArchive.class).addClass(BaseTest.class).addBeansXML();
-    }
-
-
-    @Test
-    public void shouldAddNewPlaylist() throws Exception {
-        Result result = shellTest.execute("music-add-songs --playlist " + PlaylistManager.DEFAULT_PLAYLIST + " --dir " + Paths.get("target/test-classes").toAbsolutePath(), 25, TimeUnit.SECONDS);
-        assertThat(result, not(instanceOf(Failed.class)));
-        assertThat(result.getMessage(),is(equalTo("1 song(s) added to playlist: " + PlaylistManager.DEFAULT_PLAYLIST)));
-        assertThat(playlistManager.hasPlaylist(PlaylistManager.DEFAULT_PLAYLIST), is(true));
-        Playlist playlist = playlistManager.getPlaylist(PlaylistManager.DEFAULT_PLAYLIST);
-        assertNotNull(playlist);
-        Song song = new Song(Paths.get("target/test-classes").toAbsolutePath() + "/axe.mp3");
-        assertThat(playlist.getSongs(), hasItem(song));
-
-    }
-
+	}
 
 }
