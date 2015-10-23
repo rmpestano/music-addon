d75935f21949074111fe2dc85288ab6421bb27cc
rmpestano <bigboss666>
2015-10-23 01:02:35 -0200

    readme

--- src/main/java/com/github/forge/addon/music/ui/PlayQueueCommand.java
+++ src/main/java/com/github/forge/addon/music/ui/PlayQueueCommand.java
@@ -1,21 +1,12 @@
 package com.github.forge.addon.music.ui;
 
-import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import javax.inject.Inject;
-
+import com.github.forge.addon.music.model.Song;
+import com.github.forge.addon.music.player.Player;
 import org.jboss.forge.addon.ui.command.AbstractUICommand;
 import org.jboss.forge.addon.ui.context.UIBuilder;
 import org.jboss.forge.addon.ui.context.UIContext;
 import org.jboss.forge.addon.ui.context.UIExecutionContext;
-import org.jboss.forge.addon.ui.hints.InputType;
 import org.jboss.forge.addon.ui.input.UISelectMany;
-import org.jboss.forge.addon.ui.input.UISelectOne;
-import org.jboss.forge.addon.ui.input.ValueChangeListener;
-import org.jboss.forge.addon.ui.input.events.ValueChangeEvent;
 import org.jboss.forge.addon.ui.metadata.UICommandMetadata;
 import org.jboss.forge.addon.ui.metadata.WithAttributes;
 import org.jboss.forge.addon.ui.result.Result;
@@ -23,27 +14,27 @@ import org.jboss.forge.addon.ui.result.Results;
 import org.jboss.forge.addon.ui.util.Categories;
 import org.jboss.forge.addon.ui.util.Metadata;
 
-import com.github.forge.addon.music.model.Playlist;
-import com.github.forge.addon.music.model.Song;
-import com.github.forge.addon.music.player.Player;
-import com.github.forge.addon.music.playlist.PlaylistManager;
+import javax.inject.Inject;
+import java.io.PrintStream;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by pestano on 16/08/15.
  */
-public class ShowPlayqueueCommand extends AbstractUICommand {
+public class PlayQueueCommand extends AbstractUICommand {
 
 	@Inject
 	Player player;
 
 	@Inject
-	@WithAttributes(label = "Songs", description = "Play queue songs", enabled = false, note = "If you have random configured the order will not be respected")
+	@WithAttributes(label = "Songs", description = "Enqueue songs", enabled = false, note = "If you have random configured the order will not be respected")
 	private UISelectMany<Song> songs;
 
 	@Override
 	public UICommandMetadata getMetadata(UIContext context) {
-		return Metadata.forCommand(ShowPlayqueueCommand.class).description("Show enqueued songs that will be played")
-				.name("Music: Show play queue").category(Categories.create("Music"));
+		return Metadata.forCommand(PlayQueueCommand.class).description("Show enqueued songs that will be played")
+				.name("Music: Play queue").category(Categories.create("Music"));
 
 	}
 
